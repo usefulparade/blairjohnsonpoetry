98943f2cf4a85605412a02ede3580a7638d554ee
usefulparade <breadclip.cooperative@gmail.com>
2021-02-10 18:34:35 -0500

    changed text

--- tense/sentence.js
+++ tense/sentence.js
@@ -5,7 +5,8 @@ var progress;
 
 function setup(){
     tempo = 240;
-   sentence = "Thus it was not rare to find, on the Sunday, the tallboy on its feet by the fire, and the dressing table on its head by the bed, and the night-stool on its face by the door, and the washand-stand on its back by the window; and, on the Monday, the tallboy on its back by the bed, and the dressing table on its face by the door, and the night-stool on its back by the window and the washand-stand on its feet by the fire; and on the Tuesday"
+   sentence = "It follows that the space she made for herself , were it the most possible approximation of itself , would produce the other her , who , following her , would then make space for her , and it therefore must be true that it would also be the best possible transfer — in other words , good enough : it would not be a matter of cutting in half , dividing , mirroring , or straddling across , but in folding herself backwards again and then backwards again , a wobble between , in which she would once again find her habitat , for this was the secret to living , which she had realized while dragging her table across the hardwood floor , revealing a scratch that she had not made , but it was clear that the scratch had been made by dragging the table across the hardwood floor , and looking up , she had seen herself , had followed her , and had become an anchor , and following in the drag of the current between herselves she watched herself sit by the window , unable to put the cup of coffee on the table , which was no longer in its place , it was clear : 
+   "
    sentenceArray = sentence.split(" ");
    progress = 0;
    setTimeout(TheFirst, 800);
